16112f13a5a63144d8180733a3e0257f13c485c7
Kaushal Khachane <khachaneks22@gmail.com>
2023-07-28 00:24:30 +0530

    Maximum Subarray

---
+++ Leetcode51.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+
+        if (nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        int maxSum = nums[0];
+        int currentSum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            currentSum += nums[i];
+
+            if (currentSum > maxSum) {
+                maxSum = currentSum;
+            }
+
+            maxSum = Math.max(maxSum, currentSum);
+            if (currentSum < 0) {
+                currentSum = 0;
+            }
+        }
+        return maxSum;
+    }
+}
