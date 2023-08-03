7a3affbd8e592fda42b99f33e8594bbe4f2b7499
Kaushal Khachane <khachaneks22@gmail.com>
2023-08-04 01:57:40 +0530

    Adding Math Folder (Count Digit)

---
+++ Math/NumberOfDigit.java
@@ -0,0 +1,16 @@
+public class NumberOfDigit {
+
+    public static int countDigit(int num) {
+        int count = 0;
+        while (num != 0) {
+            num = num / 10;
+            count++;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(countDigit(123));
+
+    }
+}
