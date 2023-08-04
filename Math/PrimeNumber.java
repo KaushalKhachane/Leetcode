116a86743e46f09339d5b32f547401de966873dc
Kaushal Khachane <khachaneks22@gmail.com>
2023-08-04 19:47:48 +0530

    Updating PtimeNumber

--- Math/PrimeNumber.java
+++ Math/PrimeNumber.java
@@ -26,10 +26,30 @@ public class PrimeNumber {
         return true;
     }
 
+    public static boolean isPrimeNumber(int num) {
+        if (num == 1) {
+            return false;
+        }
+        if (num == 2 || num == 3) {
+            return true;
+        }
+        if (num % 2 == 0 || num % 3 == 0) {
+            return false;
+        }
+        for (int i = 5; i * i <= num; i += 6) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         System.out.println(isPrime(13));
         System.out.println(isPrimeNum(13));
         System.out.println(isPrimeNum(14));
+        System.out.println(isPrimeNumber(13));
+        System.out.println(isPrimeNumber(22));
 
     }
 }
