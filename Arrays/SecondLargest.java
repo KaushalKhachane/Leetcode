913812e88190d07f946e88d9f1583f4205ee6b50
Kaushal Khachane <khachaneks22@gmail.com>
2023-08-13 08:14:21 +0530

    Efficient SecondLargest.java

--- Arrays/SecondLargest.java
+++ Arrays/SecondLargest.java
@@ -29,9 +29,35 @@ public class SecondLargest {
         return sLargest;
     }
 
+    // efficient approach
+    // a[i] > a[largest] = res = largest and largest
+    // a[i] == a[largest] ignore this case
+    // a[i] < a[largest]
+    // | below three case for the second largest element only
+    // |
+    // res == -1: res = i
+    // a[i] <= a[res] : ignore
+    // a[i] > a[res] : res = i
+
+    public static int sLargestEle(int arr[]) {
+        int res = -1, largest = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > arr[largest]) {
+                res = largest;
+                largest = i;
+            } else if (arr[i] != arr[largest]) {
+                if (res == -1 || arr[i] > arr[res]) {
+                    res = i;
+                }
+            }
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         int[] arr = { 10, 20, 30, 4, 50 };
         System.out.println("Largest Element Index: " + greatest(arr));
         System.out.println("Second Largest Element Index: " + secondGreatest(arr));
+        System.out.println("Second Largest: " + sLargestEle(arr));
     }
 }
