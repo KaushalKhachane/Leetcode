dbe1269283480c2820f2e7f044f7bcb618af4080
Kaushal Khachane <khachaneks22@gmail.com>
2023-08-13 23:45:54 +0530

    Que 268

---
+++ Leetcode268.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i) {
+                return i;
+            }
+        }
+        return nums.length; // If no missing number is found, return the next integer in sequence.
+    }
+}
