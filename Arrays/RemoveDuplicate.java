994d90a71079207bc810edee0bb807265f0a8c50
Kaushal Khachane <khachaneks22@gmail.com>
2023-08-14 08:27:29 +0530

    Duplicate Removed From Array

--- Arrays/RemoveDuplicate.java
+++ Arrays/RemoveDuplicate.java
@@ -1,4 +1,6 @@
 public class RemoveDuplicate {
+
+    // required theta n auxi space
     public static int removeDuplicate(int arr[]) {
         int res = 1;
         int temp[] = new int[arr.length];
@@ -17,8 +19,22 @@ public class RemoveDuplicate {
         return res;
     }
 
+    // size complexity reduced (inplace)
+    public static int removeDup(int arr[]) {
+        int res = 1;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[res - 1]) {
+                arr[res] = arr[i];
+                res++;
+            }
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         int arr[] = { 10, 10, 20, 20, 20, 30, 30, 30, 30 };
+        int arr2[] = { 10, 20, 20, 30, 30, 30 };
         System.out.println("Size without dupicate element: " + removeDuplicate(arr));
+        System.out.println(removeDup(arr2));
     }
 }
