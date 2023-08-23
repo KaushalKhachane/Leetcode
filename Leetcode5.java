4beb79b3474e6882caff51c5591df30c3aefd0ff
Kaushal Khachane <khachaneks22@gmail.com>
2023-08-23 23:42:24 +0530

    Leetcode 5

--- Leetcode5.java
+++ Leetcode5.java
@@ -1,34 +1,50 @@
 class Solution {
     public String longestPalindrome(String s) {
-        String result = "";
-        for (int i = 0; i < s.length(); i++) {
-            List<Integer> indexes = getAllIndexes(s.substring(i), s.charAt(i), i);
-            for (Integer index: indexes) {
-                if (isPalindrom(s.substring(i, index + 1))) {
-                    if (result.length() < s.substring(i, index + 1).length()) {
-                        result = s.substring(i, index + 1);
-                    }
+        if(s.length() <= 1){
+            return s;
+        }
+
+        int max_length = 1;
+        int start = 0, end = 0;
+        int n = s.length();
+        //odd length
+        for(int i = 0; i < n-1; ++i){
+            int l = i; int r = i;
+            while(l >=0 && r < n){
+                if(s.charAt(l) == s.charAt(r)){
+                    l--;
+                    r++;
+                }else{
                     break;
                 }
             }
-        }
-        return result;
-    }
-    private boolean isPalindrom(String s) {
-        for (int i = 0; i < s.length() / 2; i++ ) {
-            if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
-                return false;
+            int len = (r-l)-1;
+            if(len > max_length){
+                max_length = len;
+                start = l+1;
+                end = r-1;
             }
         }
-        return true;
-    }
-    private List<Integer> getAllIndexes(String s, char letter, int offset) {
-        List<Integer> indexes = new ArrayList();
-        for (int i = s.length() - 1; i >= 0; i--) {
-            if (s.charAt(i) == letter) {
-                indexes.add(i + offset);
+
+        // even length
+        for(int i = 0; i < n-1; ++i){
+            int l = i; int r = i+1;
+            while(l >=0 && r < n){
+                if(s.charAt(l) == s.charAt(r)){
+                    l--;
+                    r++;
+                }else{
+                    break;
+                }
+            }
+            int len = (r-l)-1;
+            if(len > max_length){
+                max_length = len;
+                start = l+1;
+                end = r-1;
             }
         }
-        return indexes;
+
+        return s.substring(start, end+1);
     }
 }
