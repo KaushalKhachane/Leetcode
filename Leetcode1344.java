a1cb4c87d6ef1211afeb8d90c46a65ff17e914a8
Kaushal Khachane <khachaneks22@gmail.com>
2023-08-29 22:12:58 +0530

    Angle between hours and minutes

---
+++ Leetcode1344.java
@@ -0,0 +1,10 @@
+class Solution {
+    public double angleClock(int hour, int minutes) {
+        float hours = (hour%12 + (float)minutes/60)*30; // 360/12
+        int minute = minutes*6; // 360/60
+
+        float angle = Math.abs(hours-minute);
+        if(angle > 180) angle = 360 - angle;
+        return angle;
+    }
+}
