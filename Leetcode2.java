4d8ec371c9b18ba8cf4aabe97324861ed1fb8296
Kaushal Khachane <khachaneks22@gmail.com>
2023-09-02 23:42:49 +0530

    Add Two Numbers Using LL

--- Leetcode2.java
+++ Leetcode2.java
@@ -11,40 +11,29 @@
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) 
     {
-        ListNode head = null;
-        ListNode temp = null;
+            ListNode dummyNode = new ListNode();
+            ListNode temp = dummyNode;
+            int carry = 0;
 
-        int carry = 0;
+            while(l1 !=  null || l2 != null || carry == 1){
+                int sum = 0;
+                if(l1 != null){
+                    sum += l1.val;
+                    l1 = l1.next;
+                }
 
-        while(l1 != null || l2 != null){
-            int sum = carry;
+                if(l2 != null){
+                    sum += l2.val;
+                    l2 = l2.next;
+                }
 
-            if(l1 != null){
-                sum += l1.val;
-                l1 = l1.next;
-            }
-
-            if(l2 != null){
-                sum += l2.val;
-                l2 = l2.next;
-            }
-
-            ListNode node = new ListNode(sum % 10);
-            carry = sum / 10;
-
-            if(temp == null){
-                temp = head = node;
-            }else{
+                sum += carry;
+                carry = sum / 10;
+                ListNode node = new ListNode(sum % 10);
                 temp.next = node;
                 temp = temp.next;
             }
 
-        }  
-
-        if (carry > 0) {
-            temp.next = new ListNode(carry);
-        } 
-
-        return head;    
+            return dummyNode.next;
     }
 }
