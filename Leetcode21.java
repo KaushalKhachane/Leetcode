3941168f4ddcfb052f6a89260f4839a6abefe34a
Kaushal Khachane <khachaneks22@gmail.com>
2023-09-02 22:28:32 +0530

    Merge Two Sorted LL

--- Leetcode21.java
+++ Leetcode21.java
@@ -10,21 +10,35 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode prehead = new ListNode(-1);
-        ListNode cur = prehead;
+        // inplace algorithm 
+        // time complexity O(n)
+        //iterative
 
-        while (l1 != null && l2 != null) {
-            if (l1.val <= l2.val) {
-                cur.next = l1;
-                l1 = l1.next;
-            } else {
-                cur.next = l2;
-                l2 = l2.next;
-            }
-            cur = cur.next;
+        if(l1 == null) return l2;
+        if(l2 == null) return l1;
+
+        if(l1.val > l2.val){
+            ListNode temp = l1;
+            l1 = l2;
+            l2 = temp;
         }
 
-        cur.next = l1 == null ? l2 : l1;
-        return prehead.next;
+        ListNode res = l1;
+        while(l1 != null && l2 != null){
+            ListNode tmp = null;
+            while(l1 != null && l1.val <= l2.val){
+                tmp = l1;
+                l1 = l1.next;
+            }
+            tmp.next = l2;
+
+            //swap
+            ListNode temp = l1;
+            l1 = l2;
+            l2 = temp;
+
+        }
+        return res;
+
     }
 }
