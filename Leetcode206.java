fe961a62b11c723fae52ddf9a23c6c1dadecd5d4
Kaushal Khachane <khachaneks22@gmail.com>
2023-09-04 12:05:53 +0530

    Reverse LL

--- Leetcode206.java
+++ Leetcode206.java
@@ -20,15 +20,21 @@ class Solution {
 
         // return dummyNode;
 
-        ListNode dummyNode = null;
-        ListNode next = null;
-        while(head != null){
-            next = head.next;
-            head.next = dummyNode;
-            dummyNode = head;
-            head = next;
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode next;
+
+        if(head == null || head.next == null){
+            return head;
         }
 
-        return dummyNode;
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
     }
 }
