610c7792cfc545601d5d9eb33400586d73e2baca
Kaushal Khachane <khachaneks22@gmail.com>
2023-09-16 22:27:39 +0530

    Length of the longest subarray with zero Sum

---
+++ Arrays/LenOfLongestSubArrayZero.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+
+public class LenOfLongestSubArrayZero {
+    // Length of the longest subarray with zero Sum
+    public static int maxLen(int A[], int n) {
+        // Your code here
+        HashMap<Integer, Integer> mpp = new HashMap<Integer, Integer>();
+
+        int maxi = 0;
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            sum += A[i];
+
+            if (sum == 0) {
+                maxi = i + 1;
+            } else {
+                if (mpp.get(sum) != null) {
+
+                    maxi = Math.max(maxi, i - mpp.get(sum));
+                } else {
+
+                    mpp.put(sum, i);
+                }
+            }
+        }
+        return maxi;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {10,0,0,0,0,0,20,30,40,50};
+        System.out.println(maxLen(arr,arr.length));
+    }
+}
