6ef2c6a19ff368d7b4e65edd0a7cd160acb62986
Kaushal Khachane <khachaneks22@gmail.com>
2023-09-17 21:17:25 +0530

    Power of x to n

--- Leetcode50.java
+++ Leetcode50.java
@@ -1,33 +1,25 @@
 class Solution {
     public double myPow(double x, int n) {
-        if (n == 0) {
-            return 1;
+        double ans = 1.0;
+        long  nn = n;
+
+        if(nn < 0) nn = -1 * nn;
+
+        while(nn > 0){
+            //if nn is odd then ans = x * ans and decrement nn by 
+            if(nn % 2 == 1){
+                ans = x * ans;
+                nn = nn - 1; 
+            }else{ // if nn is even 
+                x = x * x;
+                nn = nn / 2;
+            }
         }
-        
-        if (n < 0) {
-            x = 1 / x;
-            n = -n;
+        //if n s negative then simply 1/ans (reciprocal)
+        if(n  < 0){
+            ans = (double)1.0 / (double)(ans);
         }
-        if(x==-1.00000 && n==Integer.MIN_VALUE){
-            return Math.abs(x);
-        }
-        int even = n / 2;
-        int odd = (n + 1) / 2;
-        
-        return (helper(x, even) * helper(x, odd));
-    }
-    
-    static double helper(double num, int n) {
-        if (n == 0) {
-            return 1;
-        }
-        
-        double temp = helper(num, n / 2);
-        
-        if (n % 2 == 0) {
-            return temp * temp;
-        } else {
-            return num * temp * temp;
-        }
+        return ans;
+
     }
 }
