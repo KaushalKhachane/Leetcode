11d7bc97e14b535e08f3c81971882e40bd6d77a1
Kaushal Khachane <khachaneks22@gmail.com>
2023-09-26 18:06:41 +0530

    LeftMost Repeating Character

--- String/LeftMostRepeatingChar.java
+++ String/LeftMostRepeatingChar.java
@@ -1,7 +1,22 @@
 import java.util.Arrays;
 
 public class LeftMostRepeatingChar {
+    
     public static final int CHAR = 256;
+
+    // order of n square
+    public static final int leftMostChar(String str) {
+        for(int i = 0; i < str.length(); i++) {
+            for(int j = i + 1 ; j < str.length(); j++) {
+                if(str.charAt(i) ==  str.charAt(j)){
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+    
+    // order of n
     public static int leftMost(String str){
         int count[] = new int[CHAR];
         Arrays.fill(count,-1);
@@ -17,7 +32,22 @@ public class LeftMostRepeatingChar {
         }
         return(res==Integer.MAX_VALUE) ? Integer.MAX_VALUE : res;
     }
+
+    // traverse from right to left
+    public static int rightToLeft(String str){
+        boolean visited[] = new boolean[CHAR];
+        int res = -1;
+
+        for(int i = str.length()-1; i > 0; i--){
+            if(visited[str.charAt(i)]){
+                res = i;
+            }else{
+                visited[str.charAt(i)] = true;
+            }
+        }
+        return res;
+    }
     public static void main(String[] args) {
-        System.out.println(leftMost("Kaushal"));
+        System.out.println(rightToLeft("Kaushal"));
     }
 }
