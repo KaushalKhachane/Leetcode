4289926112cef425d10dd6e4ad48c18655bb664d
Kaushal Khachane <khachaneks22@gmail.com>
2023-09-28 13:05:59 +0530

    Group Anagrams

--- Leetcode49.java
+++ Leetcode49.java
@@ -1,20 +1,40 @@
 
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> map = new HashMap<>();
+        
 
-        for (String word : strs) {
-            char[] chars = word.toCharArray();
-            Arrays.sort(chars);
+        Map<Map<Character, Integer>, List<String>> mapofmap = new HashMap<>();
 
-            String sortedWord = new String(chars);
+        for(String word : strs){
+            Map<Character, Integer> charCountKey = new HashMap<>();
 
-            if (!map.containsKey(sortedWord)) {
-                map.put(sortedWord, new ArrayList<>());
+            for(char c : word.toCharArray()){
+                charCountKey.put(c, charCountKey.getOrDefault(c,0)+1);
             }
-            map.get(sortedWord).add(word);
+
+            if(!mapofmap.containsKey(charCountKey)){
+                mapofmap.put(charCountKey, new ArrayList<>());
+            }
+
+            mapofmap.get(charCountKey).add(word);
+
         }
 
-        return new ArrayList<>(map.values());
+        return new ArrayList<>(mapofmap.values());
+
+        // Map<String, List<String>> map = new HashMap<>();
+
+        // for(String word : strs){
+        //     char[] chars = word.toCharArray();
+        //     Arrays.sort(chars);
+        //     String sortedWord = new String(chars);
+
+        //     if(!map.containsKey(sortedWord)){
+        //         map.put(sortedWord, new ArrayList<>());
+        //     }
+        //     map.get(sortedWord).add(word);
+        // }    
+
+        // return new ArrayList<>(map.values());    
     }
 }
