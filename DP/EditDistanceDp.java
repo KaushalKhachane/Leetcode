303d335078fe63f3a4ea13787626828beaecab8e
Kaushal Khachane <khachaneks22@gmail.com>
2023-10-26 19:40:48 +0530

    Updating Edit Distance Code

--- DP/EditDistanceDp.java
+++ DP/EditDistanceDp.java
@@ -1,25 +1,25 @@
 public class EditDistanceDp {
 
     public static int getDistance(String s1, String s2, int m, int n) {
-        int dp[][] = new int[m+1][n+1];
+        int dp[][] = new int[m + 1][n + 1];
 
-        for(int i = 0; i < m; i++){
+        for (int i = 0; i <= m; i++) {  // Corrected loop bounds
             dp[i][0] = i;
         }
 
-        for(int j = 0; j < n; j++){
+        for (int j = 0; j <= n; j++) {  // Corrected loop bounds
             dp[0][j] = j;
         }
 
-        for(int i = 1; i < m; i++){
-            for(int j = 1; j < n; j++){
-                if(s1.charAt(i-1) == s2.charAt(j-1)){
-                    dp[i][j] = dp[i-1][j-1];
-                }else{
-                    return 1 + Math.min(dp[i-1][j],Math.min(dp[i][j-1], dp[i-1][j-1]));
+        for (int i = 1; i <= m; i++) {  // Corrected loop bounds
+            for (int j = 1; j <= n; j++) {  // Corrected loop bounds
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = 1 + Math.min(dp[i - 1][j], Math.min(dp[i][j - 1], dp[i - 1][j - 1]));
                 }
             }
-        }        
+        }
         return dp[m][n];
     }
 
