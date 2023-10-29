d5ac045198eeb5260d0cf9ae52e575c1428b0c37
Kaushal Khachane <khachaneks22@gmail.com>
2023-10-29 15:22:15 +0530

    Postfix Evaluation

---
+++ Leetcode150.java
@@ -0,0 +1,43 @@
+import java.util.Stack;
+
+class Solution {
+    private boolean isNumber(String token) {
+        try {
+            Integer.parseInt(token);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    private int performOperation(int operand1, int operand2, String operator) {
+        switch (operator) {
+            case "+":
+                return operand1 + operand2;
+            case "-":
+                return operand1 - operand2;
+            case "*":
+                return operand1 * operand2;
+            case "/":
+                return operand1 / operand2;
+            default:
+                throw new IllegalArgumentException("Invalid operator: " + operator);
+        }
+    }
+
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (String token : tokens) {
+            if (isNumber(token)) {
+                stack.push(Integer.parseInt(token));
+            } else {
+                int operand2 = stack.pop();
+                int operand1 = stack.pop();
+                int result = performOperation(operand1, operand2, token);
+                stack.push(result);
+            }
+        }
+        return stack.peek();
+    }
+}
