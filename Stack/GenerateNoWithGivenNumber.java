50db988dfdc10acf3ef1d61978c6bdbe7625e82e
Kaushal Khachane <khachaneks22@gmail.com>
2023-10-30 02:03:58 +0530

    Generate Number With Given Digit(5,6) Using Queue

---
+++ Stack/GenerateNoWithGivenNumber.java
@@ -0,0 +1,19 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class GenerateNoWithGivenNumber{
+    public static void printFirstN(int n){
+        Queue<String> q = new ArrayDeque<>();
+        q.offer("5");
+        q.offer("6");
+        for(int i = 0; i < n; i++){
+            String cuString = q.poll();
+            System.out.print(cuString+" ");
+            q.offer(cuString + "5");
+            q.offer(cuString + "6");
+        }
+    }
+    public static void main(String[] args) {
+     printFirstN(10);   
+    }
+}
